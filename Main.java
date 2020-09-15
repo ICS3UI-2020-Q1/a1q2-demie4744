cc546f0adc1da78182a845cd9472e3361f7376e3
demie4744 <demie4744@wrdsb.ca>
2020-09-15 16:15:34 +0000

    I made it divide 2 integers

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * this program calculates integers
+ * @author Erkam
  */
 public class Main {
 
@@ -9,7 +10,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //// Creates a Scanner used for input
+   Scanner input = new Scanner(System.in);
+
+    System.out.println("Please enter in two integers, on seperate lines, to divide.");
+    // declare and initlize the integers
+   
+ 
+
+
+ // declare and calculate variable for integer
+ System.out.println("10/3 is 3 with a remainder of 1.");
+ 
   }
 }
